a24e3894b1aa2528365e24b8f476db9fb19aa2e4
Mohit Gera <mohitgera7714@gmail.com>
2024-12-10 15:21:32 +0530

    Created Listing Module

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
+const Listing = require("./models/listing.js")
 
 //DB Connection
-const MONGO_URL = "mongodb://127.0.0.1:27017/test";
+const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
 async function main() {
     await mongoose.connect(MONGO_URL);
 }
@@ -15,10 +16,23 @@ main()
     .catch(err => console.log(err));
 
 
-
 app.get("/", (req, res) => {
     res.send("Hi , i'm Root");
 })
+
+app.get("/testlisting", async (req,res)=>{
+   let sampleListing = new Listing({
+    title:"My new Villa",
+    description:"By the beach",
+    price:1200,
+    location:"Goa",
+    country:"India"
+   }); 
+    await sampleListing.save();
+    console.log("sample was saved");
+    res.send("Successfully Tested");
+});
+
 
 // Starting Server
 app.listen(8080, () => {
